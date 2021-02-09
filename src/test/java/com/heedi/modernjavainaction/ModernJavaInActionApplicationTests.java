39b53f7cc53a54d84a82994c8d2792954bad71dd
sefide <sefide@naver.com>
2021-02-09 22:25:39 +0900

    init

---
+++ src/test/java/com/heedi/modernjavainaction/ModernJavaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heedi.modernjavainaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
