a167bb3c991053ce46ce5b4efe8f02a970e618c7
sefide <sefide@naver.com>
2021-02-11 21:45:01 +0900

    feat: add FilteringApples Class

--- src/main/java/com/heedi/modernjavainaction/chapter01/FilteringApples.java
+++ src/main/java/com/heedi/modernjavainaction/chapter01/FilteringApples.java
@@ -1,4 +1,75 @@
 package com.heedi.modernjavainaction.chapter01;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class FilteringApples {
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, "green"),
+                new Apple(155, "green"),
+                new Apple(120, "red")
+        );
+
+        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        List<Apple> greenApples = parallelFilterApples(inventory, FilteringApples::isGreenApple);
+        System.out.println(greenApples);
+
+        // [Apple{color='green', weight=155}]
+        List<Apple> heavyApples = filterApples(inventory, FilteringApples::isHeavyApple);
+        System.out.println(heavyApples);
+
+        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        List<Apple> greenApples2 = filterApples(inventory, (Apple a) -> "green".equals(a.getColor()));
+        System.out.println(greenApples2);
+
+        // [Apple{color='green', weight=155}]
+        List<Apple> heavyApples2 = filterApples(inventory, (Apple a) -> a.getWeight() > 150);
+        System.out.println(heavyApples2);
+
+        // []
+        List<Apple> weirdApples = filterApples(inventory, (Apple a) -> a.getWeight() < 80 || "brown".equals(a.getColor()));
+        System.out.println(weirdApples);
+
+    }
+
+    private static boolean isGreenApple(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+
+    private static boolean isHeavyApple(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+
+    private static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> predicate) {
+        return inventory.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Apple> parallelFilterApples(List<Apple> inventory, Predicate<Apple> predicate) {
+        return inventory.parallelStream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    public static class Apple {
+        private int weight;
+        private String color;
+
+        public Apple(int weight, String color) {
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public String getColor() {
+            return color;
+        }
+    }
 }
