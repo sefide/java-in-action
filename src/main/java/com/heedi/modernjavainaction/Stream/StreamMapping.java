f0a12f4b4281dbb86a80238159aa8f90e79d4d83
sefide <sefide@naver.com>
2021-02-17 23:16:52 +0900

    refactor: quiz6

--- src/main/java/com/heedi/modernjavainaction/Stream/StreamMapping.java
+++ src/main/java/com/heedi/modernjavainaction/Stream/StreamMapping.java
@@ -1,11 +1,8 @@
 package com.heedi.modernjavainaction.Stream;
 
-import org.springframework.util.CollectionUtils;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StreamMapping {
 
@@ -20,18 +17,16 @@ public class StreamMapping {
         List<Integer> pair1 = Arrays.asList(1, 2, 3);
         List<Integer> pair2 = Arrays.asList(3, 4);
 
-        List<List<Integer>> pair = pair1.stream()
-                .map(a -> pair2.stream().map(b -> Arrays.asList(a, b)).collect(Collectors.toList()))
-                .flatMap(List::stream)
+        List<int[]> pair = pair1.stream()
+                .flatMap(a -> pair2.stream()
+                        .map(b -> new int[]{a, b}))
                 .collect(Collectors.toList());
 
         // Quiz6 - 3
-        List<List<Integer>> threePair = pair1.stream()
-                .map(a -> pair2.stream().map(b -> Arrays.asList(a, b)).collect(Collectors.toList()))
-                .flatMap(List::stream)
-                .filter(a -> a.stream().reduce(Integer::sum).get() % 3 == 0)
+        List<int[]> threePair = pair1.stream()
+                .flatMap(a -> pair2.stream()
+                        .filter(b -> (a + b) % 3 == 0)
+                        .map(b -> new int[]{a, b}))
                 .collect(Collectors.toList());
-
-        System.out.println();
     }
 }
