984da79060ac5c415c15c74daf6c59f431aa4396
sefide <sefide@naver.com>
2021-02-18 22:00:43 +0900

    refactor: 실전 연습 문제

--- src/main/java/com/heedi/modernjavainaction/Stream/ActualPractice.java
+++ src/main/java/com/heedi/modernjavainaction/Stream/ActualPractice.java
@@ -3,6 +3,7 @@ package com.heedi.modernjavainaction.Stream;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ActualPractice {
@@ -32,45 +33,50 @@ public class ActualPractice {
         transactions.stream()
                 .map(Transaction::getTrader)
                 .map(Trader::getCity)
-                .distinct()
+                .distinct() // toSet으로 반환 시, 자동 중복 제
                 .forEach(System.out::println);
 
         // 케임브리지 근무하는 모든 거래자 이름순 정렬
         List<Transaction> cambridgeTraders = transactions.stream()
                 .filter(transaction -> "Cambridge".equals(transaction.getTrader().getCity()))
+                .distinct()
                 .sorted(Comparator.comparing(t -> t.getTrader().getName()))
                 .collect(Collectors.toList());
 
         // 모든 거래자 이름순 정렬
-        transactions.stream()
+        String names = transactions.stream()
                 .map(Transaction::getTrader)
                 .map(Trader::getName)
                 .distinct()
-                .sorted(String::compareTo)
-                .forEach(System.out::println);
+                .sorted()
+                .reduce("", (n1, n2) -> n1 + n2);
+        System.out.println(names);
 
         // 밀라노에 거래자가 있는지
         boolean hasMilanTrader = transactions.stream()
-                .map(Transaction::getTrader)
-                .anyMatch(t -> "Milan".equals(t.getCity()));
+                .anyMatch(t -> "Milan".equals(t.getTrader().getCity()));
 
         // 케임브리지에 거주하는 거래자의 모든 트랜잭션값을 출력
         transactions.stream()
                 .filter(transaction -> "Cambridge".equals(transaction.getTrader().getCity()))
+                .map(Transaction::getValue)
                 .forEach(System.out::println);
 
         // 전체 트랜잭션 중 최댓값
-        int max = transactions.stream()
+        Optional<Integer> max = transactions.stream()
                 .map(Transaction::getValue)
-                .reduce(Integer::max)
-                .get();
+                .reduce(Integer::max);
 
         // 전체 트랜잭션 중 최솟값
-        int min = transactions.stream()
+        Optional<Integer> min = transactions.stream()
                 .map(Transaction::getValue)
-                .reduce(Integer::min)
-                .get();
+                .reduce(Integer::min);
+
+        // 최댓값 Comparator 함수 사용한 방법
+        Optional<Transaction> maxUsingComparator = transactions.stream()
+                .max(Comparator.comparing(Transaction::getValue));
 
         System.out.println("max : " + max + ", min : " + min);
+        System.out.println("optional max : " + maxUsingComparator);
     }
 }
