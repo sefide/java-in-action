5a1b6fe883448e4eba8b89456bdbcc6310bc5838
sefide <sefide@naver.com>
2021-02-22 22:28:20 +0900

    feat: add collect summing

--- src/main/java/com/heedi/modernjavainaction/stream/CollectReducing.java
+++ src/main/java/com/heedi/modernjavainaction/stream/CollectReducing.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.minBy;
 
 public class CollectReducing {
 
@@ -19,6 +20,15 @@ public class CollectReducing {
         Optional<Dish> mostCalorieDish2 = menu.stream()
                 .max(dishCaloriesComparator);
 
+        Optional<Dish> leastCalorieDish = menu.stream()
+                .collect(minBy(dishCaloriesComparator));
+
+        Optional<Dish> leastCalorieDish2 = menu.stream()
+                .min(dishCaloriesComparator);
+
         System.out.println(mostCalorieDish);
+        System.out.println(mostCalorieDish2);
+        System.out.println(leastCalorieDish);
+        System.out.println(leastCalorieDish2);
     }
 }
--- src/main/java/com/heedi/modernjavainaction/stream/CollectSumming.java
+++ src/main/java/com/heedi/modernjavainaction/stream/CollectSumming.java
@@ -1,11 +1,36 @@
 package com.heedi.modernjavainaction.stream;
 
 import java.util.List;
+import java.util.LongSummaryStatistics;
+
+import static java.util.stream.Collectors.*;
 
 public class CollectSumming {
 
     public static void main(String[] args) {
         List<Dish> menu = Dish.MENU_SAMPLE;
 
+        // 합계
+        int totalCalories = menu.stream()
+                .collect(summingInt(Dish::getCalories));
+
+        int totalCalories2 = menu.stream()
+                .mapToInt(Dish::getCalories)
+                .sum();
+
+        System.out.println(totalCalories);
+        System.out.println(totalCalories2);
+
+        // 평균
+        double averageCalories = menu.stream()
+                .collect(averagingDouble(Dish::getCalories));
+
+        System.out.println(averageCalories);
+
+        // 통계
+        LongSummaryStatistics menuStatistics = menu.stream()
+                .collect(summarizingLong(Dish::getCalories));
+
+        System.out.println(menuStatistics);
     }
 }
