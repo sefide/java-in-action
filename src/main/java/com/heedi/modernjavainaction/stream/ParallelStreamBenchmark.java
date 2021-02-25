d5e6a4d2e1ccba7fc7a0b28337e8411a592644fb
sefide <sefide@naver.com>
2021-02-25 23:07:13 +0900

    feat: add Faild ParallelStreamBenchMark Class !!!!!

--- src/main/java/com/heedi/modernjavainaction/stream/ParallelStreamBenchmark.java
+++ src/main/java/com/heedi/modernjavainaction/stream/ParallelStreamBenchmark.java
@@ -1,4 +1,27 @@
 package com.heedi.modernjavainaction.stream;
 
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(value = 2, jvmArgs = {"-Xms4G", "-Xmx4G"})
 public class ParallelStreamBenchmark {
+    private static final long N = 10_000_000L;
+
+    // 벤치마크 대상 메서드
+    @Benchmark
+    public long sequentialSum() {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(N)
+                .reduce(0L, Long::sum);
+    }
+
+    // 벤치마크 실행할 때마다 가비지 컬렉터 동작 시도
+    @TearDown(Level.Invocation)
+    public void tearDown() {
+        System.gc();
+    }
 }
