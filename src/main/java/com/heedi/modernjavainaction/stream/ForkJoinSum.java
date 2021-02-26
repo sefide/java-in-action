00dfc518b73f4a3d7b1b8f2568243e5a0a0f07b7
sefide <sefide@naver.com>
2021-02-26 23:11:41 +0900

    feat: add ForkJoinSumCalculator

---
+++ src/main/java/com/heedi/modernjavainaction/stream/ForkJoinSum.java
@@ -0,0 +1,24 @@
+package com.heedi.modernjavainaction.stream;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.stream.LongStream;
+
+public class ForkJoinSum {
+
+    public static void main(String[] args) {
+        long n = 5L;
+        long result = forkJoinSum(n);
+
+        System.out.println(result);
+    }
+
+    private static long forkJoinSum(long n) {
+        long[] numbers = LongStream.rangeClosed(1, n)
+                .toArray();
+
+        ForkJoinTask<Long> task = new ForkJoinSumCalculator(numbers);
+
+        return new ForkJoinPool().invoke(task); // task의 결과값 반환
+    }
+}
