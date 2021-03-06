8c3a93528096b4ca173106d30fb49cf76a49c280
sefide <sefide@naver.com>
2021-03-06 14:56:01 +0900

    feat: Optional filter

--- src/main/java/com/heedi/modernjavainaction/optional/OptionalMain.java
+++ src/main/java/com/heedi/modernjavainaction/optional/OptionalMain.java
@@ -7,8 +7,8 @@ import java.util.stream.Collectors;
 public class OptionalMain {
 
     public static void main(String[] args) {
-        Person person = new Person(new Car(new Insurance("in")));
-        Person nullablePerson = new Person(new Car(null));
+        Person person = new Person(new Car(new Insurance("in")), 10);
+        Person nullablePerson = new Person(new Car(null), 40);
 
         System.out.println(getCarInsuranceName(Optional.of(person)));
         System.out.println(getCarInsuranceName(Optional.of(nullablePerson)));
@@ -29,6 +29,40 @@ public class OptionalMain {
                 .map(optIns -> optIns.map(Insurance::getName))
                 .flatMap(Optional::stream)
                 .collect(Collectors.toList());
+    }
+
+    public static Optional<Insurance> nullSafeFindCheapestInsurance(Optional<Person> person, Optional<Car> car) {
+        if (person.isPresent() && car.isPresent()) {
+            return Optional.of(findCheapestInsurance(person.get(), car.get()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Insurance> nullSafeFindCheapestInsuranceUnWrap(Optional<Person> person, Optional<Car> car) {
+        // person.map을 실행했다면 Optional<Optional<Insurance>>를 반환해야 할 것이다.
+        return person.flatMap(p -> car.map(c -> findCheapestInsurance(p, c)));
+    }
+
+    private static Insurance findCheapestInsurance(Person p, Car c) {
+        Insurance cheapestInsurance = new Insurance("cheap");
+
+        return cheapestInsurance;
+    }
+
+    private static void isCambridgeInsurance(Insurance insurance) {
+        Optional.ofNullable(insurance)
+                .filter(i -> "CambridgeInsurance".equals(i.getName()))
+                .ifPresent(x -> System.out.println("Ok"));
+    }
+
+    public static String getMinAgeCarInsuranceName(Optional<Person> person, int minAge) {
+        return person
+                .filter(p -> p.getAge() >= minAge)
+                .flatMap(Person::getCar)
+                .flatMap(Car::getInsurance)
+                .map(Insurance::getName)
+                .orElse("Unknown");
     }
 
     // 중첩 Optional이 생성되어 컴파일 실패
--- src/main/java/com/heedi/modernjavainaction/optional/Person.java
+++ src/main/java/com/heedi/modernjavainaction/optional/Person.java
@@ -4,13 +4,18 @@ import java.util.Optional;
 
 public class Person {
     private final Optional<Car> car;
+    private final int age;
 
-    public Person(Car car) {
+    public Person(Car car, int age) {
         this.car = Optional.ofNullable(car);
+        this.age = age;
     }
 
     public Optional<Car> getCar() {
         return car;
     }
 
+    public int getAge() {
+        return age;
+    }
 }
